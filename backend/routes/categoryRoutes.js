e1893a6e8f175f0d71d12b3192b62f10141a41bf
Ayush Acharjya <ayushacharjya@gmail.com>
2024-07-06 12:41:02 +0100

    create routes form create, update, delete, read categories

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,8 +1,22 @@
 import express from "express";
 import { authenticate, authoriseAdmin } from "../middlewares/authMiddleware.js";
+import {
+  createCategory,
+  updateCategory,
+  removeCategory,
+  listCategory,
+  readCategory,
+} from "../controllers/categoryController.js";
 
 const router = express.Router();
 
-router.route("/").post(createCategory);
+router.route("/").post(authenticate, authoriseAdmin, createCategory);
+router.route("/:categoryId").put(authenticate, authoriseAdmin, updateCategory);
+router
+  .route("/:categoryId")
+  .delete(authenticate, authoriseAdmin, removeCategory);
+
+router.route("/categories").get(listCategory);
+router.route("/:id").get(readCategory);
 
 export default router;
