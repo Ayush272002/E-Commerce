ab2b2b705e4ed87d9143877f4ffee3c20005c2b4
Ayush Acharjya <ayushacharjya@gmail.com>
2024-07-09 00:12:31 +0100

    fix typo

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -12,22 +12,25 @@ const reviewSchema = mongoose.Schema(
       ref: "User",
     },
   },
-  { timestams: true }
+  { timestamps: true }
 );
 
-const productSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  image: { type: String, required: true },
-  brand: { type: String, required: true },
-  quantity: { type: Number, required: true },
-  category: { type: ObjectId, ref: "Category", required: true },
-  description: { type: String, required: true },
-  review: [reviewSchema],
-  rating: { type: Number, required: true, default: 0 },
-  numReviews: { type: Number, required: true, default: 0 },
-  price: { type: Number, required: true, default: 0 },
-  countInStock: { type: Number, required: true, default: 0 },
-}, { timestamps: true });
+const productSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    brand: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    category: { type: ObjectId, ref: "Category", required: true },
+    description: { type: String, required: true },
+    review: [reviewSchema],
+    rating: { type: Number, required: true, default: 0 },
+    numReviews: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
+    countInStock: { type: Number, required: true, default: 0 },
+  },
+  { timestamps: true }
+);
 
 const Product = mongoose.model("Product", productSchema);
 export default Product;
